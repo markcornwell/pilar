af33789953eebdc28f3eda04eb0ae2bfbc9b2382
markcornwell <mark.cornwell@me.com>
2015-09-13 15:25:07 -0400

    re-enabled rest of test suite.  Passed all 472 tests.

--- compil.scm
+++ compil.scm
@@ -35,13 +35,13 @@
 
 (load "tests-driver.scm")
 (load "tests/tests-1.8-req.scm")
-;(load "tests/tests-1.7-req.scm")
-;(load "tests/tests-1.6-req.scm")
-;(load "tests/tests-1.5-req.scm")
-;(load "tests/tests-1.4-req.scm")
-;(load "tests/tests-1.3-req.scm")
-;(load "tests/tests-1.2-req.scm")
-;(load "tests/tests-1.1-req.scm")
+(load "tests/tests-1.7-req.scm")
+(load "tests/tests-1.6-req.scm")
+(load "tests/tests-1.5-req.scm")
+(load "tests/tests-1.4-req.scm")
+(load "tests/tests-1.3-req.scm")
+(load "tests/tests-1.2-req.scm")
+(load "tests/tests-1.1-req.scm")
 
 ;; utility
 (define first car)
--- pgm.s
+++ pgm.s
@@ -1,66 +1,10 @@
-# (letrec ((e (lambda (x) (if (fxzero? x) #t (o (fxsub1 x))))) (o (lambda (x) (if (fxzero? x) #f (e (fxsub1 x)))))) (e 25))
+# -536870912
 
-# emit-letrec expr=(letrec ((e (lambda (x) (if (fxzero? x) #t (o (fxsub1 x))))) (o (lambda (x) (if (fxzero? x) #f (e (fxsub1 x)))))) (e 25))
-#  bindings=((e (lambda (x) (if (fxzero? x) #t (o (fxsub1 x))))) (o (lambda (x) (if (fxzero? x) #f (e (fxsub1 x))))))
-#  lvars=(e o)
-#  lambdas=((lambda (x) (if (fxzero? x) #t (o (fxsub1 x)))) (lambda (x) (if (fxzero? x) #f (e (fxsub1 x)))))
-#  labels=("L_23" "L_22")
-#  env=((e . "L_23") (o . "L_22"))
-#  ---- >>>>> emit-lambdas start ----
-    .text
-    .align 4,0x90
-    .globl L_23
-L_23:
-    movl -4(%esp), %eax   # stk load
-    cmp $0, %eax
-    sete %al
-    movzbl %al, %eax
-    sal $6, %al
-    or $47, %al
-    cmp $47, %al
-    je L_24
-    movl $111, %eax     # immediate
-    jmp L_25
-L_24:
-    movl -4(%esp), %eax   # stk load
-    addl $-4, %eax
-    mov %eax, -12(%esp)    # arg
-    lea -4(%esp), %esp    # adjust base
-    call L_22   # app  
-    lea 4(%esp), %esp    # adjust base
-L_25:
-    ret   # from lambda
-    .text
-    .align 4,0x90
-    .globl L_22
-L_22:
-    movl -4(%esp), %eax   # stk load
-    cmp $0, %eax
-    sete %al
-    movzbl %al, %eax
-    sal $6, %al
-    or $47, %al
-    cmp $47, %al
-    je L_26
-    movl $47, %eax     # immediate
-    jmp L_27
-L_26:
-    movl -4(%esp), %eax   # stk load
-    addl $-4, %eax
-    mov %eax, -12(%esp)    # arg
-    lea -4(%esp), %esp    # adjust base
-    call L_23   # app  
-    lea 4(%esp), %esp    # adjust base
-L_27:
-    ret   # from lambda
-#  ---- <<<<<  emit-lambdas end ------
     .text
     .align 4,0x90
     .globl _L_scheme_entry
 _L_scheme_entry:
-    movl $100, %eax     # immediate
-    mov %eax, -8(%esp)    # arg
-    call L_23   # app  
+    movl $-2147483648, %eax     # immediate
     ret
     .text
     .align 4,0x90
