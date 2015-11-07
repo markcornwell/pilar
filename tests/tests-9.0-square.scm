54d65d8722ddfed44d89f5cd517f3710bf8bd5c3
Mark Cornwell <mark.cornwell@me.com>
2015-11-07 08:47:41 -0500

    syntax-extensions branch initial commit
    This branch will implement syntax extensions such as define, cond, etc.
    by program transformations on the sources.  At the same time will clean
    up the compiler passes.  Make forms like begin and funcall explicitly
    part of the transformation passes and not something interpreted on the
    fly.  Striving for clarity and ease of debugging trading off some
    compile time.  Goal of the branch is to start compiling parts e5chess
    using pilar.  Will strart building a new series of tests toward this
    end.

--- tests/tests-9.0-square.scm
+++ tests/tests-9.0-square.scm
@@ -1,5 +1,41 @@
 
 (add-tests-with-string-output "square"
-  [(begin (load "square.scm") a1) => "0\n"]
+  [(begin (define a8 #o70) a8) => "56\n"]
+  [(begin (define NSQ #o100) a8) => "64\n"]
+  [(begin
+     (define a8 #o70)
+     (define (isSquare x)
+       (and (fx<= a1 x) (fx<= x h8)))
+     (isSquare a8)) => "#t\n"]
+  [(begin
+     (define NSQ #o100)
+     (define (isSquare x)
+       (and (fx<= a1 x) (fx<= x h8)))
+     (isSquare NSQ)) => "#f\n"]
+  [(begin
+     (define NSQ #o100)
+     (define (noSquare x) (fx= x NSQ))
+     (noSquare NSQ)) => "#t\n"]
+  [(begin
+     (define a8 #o70)
+     (define (noSquare x) (fx= x NSQ))
+     (noSquare a8)) => "#f\n"]
+  [(begin
+     (define a8 #o70)
+     (define (rank sq) (fix:lsh (fix:and sq #o70) -3))
+     (rank a8)) => "7\n"]
+  [(begin
+     (define a8 #o70)
+     (define (file sq) (fix:and sq #o07)) 
+     (file a8)) => "0\n"]
+  [(begin 
+     (define (squareAt f r) (fix:+ f (fix:* 8 r)))
+     (squareAt 0 7)) => "56\n"]
+  [(begin
+     (define a8 #o70)
+     (define (squareAt f r) (fix:+ f (fix:* 8 r)))
+     (eq? (squareAt 0 7) a8)) => "#t\n"]     
+  
+  ;[(begin (load "square.scm") a1) => "0\n"]
 ) 
 
