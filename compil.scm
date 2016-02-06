30995dc6debab9c9c96150ee2ce94e1e6d1d258a
Mark Cornwell <mark.cornwell@me.com>
2016-02-06 17:33:26 -0500

    minor edits

--- compil.scm
+++ compil.scm
@@ -166,7 +166,7 @@
 ;; (load "tests/tests-4.1-req.scm")  ;; remainder modulo quotient write-char write/display
 ;; (load "tests/tests-3.4-req.scm")  ;; apply
 ;; (load "tests/tests-3.3-req.scm")  ;; string-set! errors
-;; (load "tests/tests-3.2-req.scm")  ;; error, argcheck
+;;(load "tests/tests-3.2-req.scm")  ;; error, argcheck
 (load "tests/tests-3.1-req.scm")  ;; vector
 
 (load "tests/tests-2.6-req.scm")  ;; variable arguments to lambda
