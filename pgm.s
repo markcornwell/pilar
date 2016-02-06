d4a2cf05acab0f3eb239c910b0b40ae16d9425b2
Mark Cornwell <mark.cornwell@me.com>
2016-02-06 17:53:48 -0500

    added error-argcheck tests
    Passes tests except for arg-check for primitives.  Need to figure out
    what is happening.  See bottom of tests-3.2-req.scm.

--- pgm.s
+++ pgm.s
@@ -1,24 +1,24 @@
-# -536870912
+# (cons 1 2 3)
 # == explicit-begins  ==>
-# -536870912
+# (cons 1 2 3)
 # == eliminate-let*  ==>
-# -536870912
+# (cons 1 2 3)
 # == uniquify-variables  ==>
-# -536870912
+# (cons 1 2 3)
 # == vectorize-letrec  ==>
-# -536870912
+# (cons 1 2 3)
 # == eliminate-set!  ==>
-# -536870912
+# (cons 1 2 3)
 # == close-free-variables  ==>
-# -536870912
+# (cons 1 2 3)
 # == eliminate-quote  ==>
-# -536870912
+# (cons 1 2 3)
 # == eliminate-when/unless  ==>
-# -536870912
+# (cons 1 2 3)
 # == eliminate-cond  ==>
-# -536870912
+# (cons 1 2 3)
 # == external-symbols  ==>
-# -536870912
+# (cons 1 2 3)
 # emit-scheme-entry
     .text
     .align 16, 0x90
@@ -31,25 +31,4 @@ _L_scheme_entry:
     jmp base_init
 base_init_callback:
     addl $4,%esp
-# emit-expr -536870912
-    movl $-2147483648, %eax     # immed -536870912
-    ret
-    .text
-    .align 16, 0x90
-    .globl _scheme_entry
-_scheme_entry:
-    movl 4(%esp), %ecx
-    movl %ebx, 4(%ecx)
-    movl %esi, 16(%ecx)
-    movl %edi, 20(%ecx)
-    movl %ebp, 24(%ecx)
-    movl %esp, 28(%ecx)
-    movl 12(%esp), %ebp
-    movl 8(%esp), %esp
-    call _L_scheme_entry
-    movl 4(%ecx), %ebx
-    movl 16(%ecx), %esi
-    movl 20(%ecx), %edi
-    movl 24(%ecx), %ebp
-    movl 28(%ecx), %esp
-    ret
+# emit-expr (cons 1 2 3)
